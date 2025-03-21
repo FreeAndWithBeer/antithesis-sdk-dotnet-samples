6c69d1b69155d0546394affa8731324a21b6d345
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2025-03-21 17:10:19 -0400

    Stubbed in HelloWeb ASP.NET Core barebones application with Antithesis SDK Assert calls and Source Generator for Catalog.

---
+++ HelloWeb/Program.cs
@@ -0,0 +1,19 @@
+using Antithesis.SDK;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () =>
+{
+    Assert.Always(false, "Harcoded to Fail");
+    
+    return "Hello Web!";
+});
+
+Lifecycle.SetupComplete();
+
+Assert.Reachable("Immediately After Setup Complete");
+
+app.Run();
+
+Assert.Unreachable("app.Run Blocks Indefinitely");
