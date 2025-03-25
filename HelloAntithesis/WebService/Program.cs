6a1654dc5aa14cea1af06f44dd9fe937529c70cb
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2025-03-25 10:52:16 -0400

    Changed a hardcoded Assert fail to use Random to fail 1 / 10k executions.

--- HelloAntithesis/WebService/Program.cs
+++ HelloAntithesis/WebService/Program.cs
@@ -3,9 +3,13 @@ using Antithesis.SDK;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () =>
+var random = Antithesis.SDK.Random.SharedThrowIfNativeLibraryNotExists;
+
+app.MapGet("/", async () =>
 {
-    Assert.Always(false, "Harcoded to Fail");
+    Assert.Always(random.NextDouble() < 0.9999, "Should Fail Randomly 1 of 10k");
+
+    await Task.Delay(100);
     
     return "Hello Antithesis!";
 });
